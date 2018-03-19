13480a8c09d172c290ef7f395078b695d19390c9
Aaron.Nacua <alnacua@gmail.com>
2018-03-19 16:41:28 -0500

    app1 and app2 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: ""
+    };
+  }
+  handleChange(value) {
+    this.setState({ userInput: value });
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Drills App 1</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p>{this.state.userInput}</p>
+        <input
+          placeholder="enter some text"
+          onChange={e => this.handleChange(e.target.value)}
+        />
       </div>
     );
   }
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [],
+      userInput: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addToList = this.addToList.bind(this);
+  }
+  handleChange(value) {
+    this.setState({ userInput: value });
+  }
+  addToList() {
+    var newList = this.state.list.slice();
+    newList.push(this.state.userInput);
+    this.setState({ list: newList });
+  }
   render() {
+    let { list } = this.state;
+    console.log(this.state.list);
+    let myList = list.map((list, index) => {
+      return <div key={index}>{list}</div>;
+    });
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Drills App 2</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> {this.state.userInput}</p>
+        <input
+          placeholder="Enter Item"
+          onChange={e => {
+            this.handleChange(e.target.value);
+          }}
+        />
+        <button onClick={() => this.addToList()}>Add to List</button>
+        <div>{myList.length ? myList : "Add to List"}</div>
       </div>
     );
   }
